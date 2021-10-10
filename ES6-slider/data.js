92632ee6e2af573686db320aa67d2256bf3dec61
Eglis-dogjani <64479073+Eglis-dogjani@users.noreply.github.com>
2021-10-10 12:48:16 -0700

    Create data.js

---
+++ ES6-slider/data.js
@@ -0,0 +1,25 @@
+const people = [
+  {
+    img:
+      "https://www.teksteshqip.com/img_upz/galeri_full/136/136466.jpg",
+    name: "Noizy",
+    job: "Kengetar Hip-hop",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,
+  },
+  {
+    img:
+      "https://www.teksteshqip.com/img_upz/galeri_full/191/191421.jpg",
+    name: "Finem",
+    job: "Kengetar i njohur",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+  {
+    img:
+      "https://www.teksteshqip.com/img_upz/galeri_full/182/182048.jpg",
+    name: "Capital T",
+    job: "Kengetar Shqiptar",
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
+];
+
+export default people;
